3d8fd532a9e91ddd49ead9419f789cfe459787b2
Jeremiah <jeremiah.d.wise@gmail.com>
2021-10-12 16:55:29 -0500

    Change playable cards in hand to buttons

--- src/ui/public/js/start-game.js
+++ src/ui/public/js/start-game.js
@@ -15,12 +15,17 @@ const you = document.getElementById('you')
 const yourDrawPile = document.getElementById('yourDrawPile')
 const yourHand = document.getElementById('yourHand')
 const yourDiscardPile = document.getElementById('yourDiscardPile')
+const supply = document.getElementById('supply')
+const supplyCoins = document.getElementById('supplyCoins')
+const supplyVictory = document.getElementById('supplyVictory')
 
 
 joinGameButton.addEventListener('click', addToQueue)
 leaveWaitingRoomButton.addEventListener('click', exitWaitingRoom)
 drawHandButton.addEventListener('click', drawHand)
 discardButton.addEventListener('click', toDiscardPile)
+
+
 
 
 function addToQueue () {
@@ -38,6 +43,9 @@ function exitWaitingRoom () {
 
 }
 
+function playCard (e) {
+  console.log(this.id)
+}
 
 function toDiscardPile (){
   socket.emit('discardHand', gameRoom.innerText)
@@ -62,20 +70,56 @@ socket.on('startingGame', (player) => {
   waiting.style.display = 'none'
   leaveWaitingRoomButton.style.display = 'none'
   drawHandButton.style.display = 'block'
+  supply.style.display = 'block'
   socket.emit('startingPile', gameRoom.innerText)
 })
 
 
 socket.on('updatePlayerCards', (playerCards) => {
   yourDrawPile.innerText = `Draw Pile: ${playerCards.drawPile.length} cards`
-  yourHand.innerText = `Your Hand: ${playerCards.hand}`
+  yourHand.innerText = 'Your Hand:'
+  let card_num = 0
+  playerCards.hand.forEach(card => {
+    card_num++
+    if (card[1] == 'vc') {
+      const cardElement = document.createElement('p')
+      cardElement.classList.add('playingCards')
+      cardElement.innerText = card[0]
+      yourHand.append(cardElement)
+    }
+    else {
+      let div_num = String(card_num)
+      const cardElement = document.createElement('button')
+      cardElement.id = `${div_num}`
+      cardElement.classList.add('playingCards')
+      cardElement.innerText = card[0]
+      cardElement.addEventListener('click', playCard)
+      yourHand.append(cardElement)
+    }
+  });
   yourDiscardPile.innerText = `Discard Pile: ${playerCards.discardPile.length} cards`
 })
 
 socket.on('updateOpponentCards', (opponentCards) => {
   opponentDrawPile.innerText = `Opponent Draw Pile: ${opponentCards.drawPile.length} cards`
-  opponentHand.innerText = `Opponent Hand: ${opponentCards.hand}`
+  opponentHand.innerText = `Opponent Hand: ${opponentCards.hand.length}`
   opponentDiscardPile.innerText = `Opponent Discard Pile: ${opponentCards.discardPile.length} cards`
+})
+
+socket.on('updateSupply', (supplyCards) => {
+  for (const [key, value] of Object.entries(supplyCards.coinCards)) {
+    const cardElement = document.createElement('p')
+    cardElement.innerText = `
+    ${key}: amount:${value.amount}, value:${value.value}, cost:${value.cost}`
+    supplyCoins.append(cardElement)
+  }
+  for (const [key, value] of Object.entries(supplyCards.victoryCards)) {
+    const cardElement = document.createElement('p')
+    cardElement.innerText = `
+    ${key}: amount:${value.amount}, value:${value.points}, cost:${value.cost}`
+    supplyVictory.append(cardElement)
+  }
+
 })
 
 
